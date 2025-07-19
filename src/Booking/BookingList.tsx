886ca6772ffa2f9b106c8f7da79fac54a603ade2
FIRST_NAME LAST_NAME <OUR_NAME@example.com>
2025-07-19 21:22:17 +0530

    Refactor BookingList: correct field name from 'firstname' to 'firstName' and remove unused imports and commented code

--- src/Booking/BookingList.tsx
+++ src/Booking/BookingList.tsx
@@ -1,19 +1,13 @@
 import {
-  ChipField,
   Datagrid,
   DateField,
-  DeleteWithConfirmButton,
-  EditButton,
   List,
   NumberField,
   ReferenceField,
-  ReferenceManyField,
-  SingleFieldList,
   TextField,
   useGetIdentity,
 } from "react-admin";
 import CurrencyField from "../components/CustomFields/CurrencyField";
-import StatusDropdown from "./StatusDropdown";
 
 export const BookingList = () => {
   const { data: user } = useGetIdentity();
@@ -22,14 +16,12 @@ export const BookingList = () => {
       <Datagrid rowClick={false} bulkActionButtons={false}>
         <TextField source="id" />
         <ReferenceField source="userId" reference="user">
-          <TextField source="firstname" />
+          <TextField source="firstName" />
         </ReferenceField>
         <ReferenceField source="packageId" reference="package" />
         <DateField source="bookingDate" />
         <CurrencyField source="totalPrice" />
         <NumberField source="totalSlots" />
-        {/* <EditButton variant="bootstrap" color="primary" />
-        <DeleteWithConfirmButton variant="bootstrap" color="danger" /> */}
       </Datagrid>
     </List>
   );
