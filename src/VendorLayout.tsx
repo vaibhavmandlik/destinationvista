f07370fceac0abba0a5e2046d1611859c153806e
FIRST_NAME LAST_NAME <OUR_NAME@example.com>
2025-07-21 01:19:03 +0530

    Refactor VendorLayout: add collapsible menu component and organize menu items

--- src/VendorLayout.tsx
+++ src/VendorLayout.tsx
@@ -9,7 +9,12 @@ import {
   useGetIdentity,
   useLogout,
 } from "react-admin";
-import { FaBookMedical, FaChartArea, FaChartLine, FaHome } from "react-icons/fa";
+import {
+  FaBookMedical,
+  FaChartArea,
+  FaChartLine,
+  FaHome,
+} from "react-icons/fa";
 import {
   PiEnvelopeSimpleBold,
   PiLockKeyBold,
@@ -24,6 +29,9 @@ import logo from "./assets/logo.svg";
 import { SwitchVendor } from "./SwitchVendor";
 import useHasVendors from "./hook/useHasvendors";
 
+import { useState } from "react";
+import { Box, Button, Collapse, MenuItem } from "@mui/material";
+
 // Custom Sidebar (Optional)
 export const MySidebar = ({ children }) => {
   useLocale(); // force redraw on locale change
@@ -33,12 +41,26 @@ export const MySidebar = ({ children }) => {
     </>
   );
 };
+function CollapsibleMenu({ children, ...props }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Box>
+      <Button onClick={() => setOpen((prev) => !prev)}>{props.menuName}</Button>
+      <Collapse in={open} timeout={400}>
+        <Box sx={{ border: "1px solid #ccc", borderRadius: 1, p: 2, mt: 1 }}>
+          {children}
+        </Box>
+      </Collapse>
+    </Box>
+  );
+}
 
 export const MyMenu = () => {
   const hasVendors = useHasVendors();
-  const { data: user,isLoading:isUserLoading } = useGetIdentity();
+  const { data: user, isLoading: isUserLoading } = useGetIdentity();
   const logout = useLogout();
-  const { data: VendorList,isLoading } = useGetList("vendor", {
+  const { data: VendorList, isLoading } = useGetList("vendor", {
     filter: { userId: user?.id },
   });
   const [open] = useSidebarState();
@@ -47,24 +69,24 @@ export const MyMenu = () => {
       value: item.id,
       label: item.agencytitle,
     })) || [];
-    // const approvedVendors = VendorList?.filter(
-    //   (vendor: any) => vendor.isApproved === "1"
-    // );
-    // useEffect(() => {
-    //   if (isLoading) return;
-    //   debugger;
-    //   if (data.length === 1 && approvedVendors?.length == 0) {
-    //     debugger;
-    //     logout();
-    //   }
-    // }, [data,isLoading]);
-    useEffect(() => {
+  // const approvedVendors = VendorList?.filter(
+  //   (vendor: any) => vendor.isApproved === "1"
+  // );
+  // useEffect(() => {
+  //   if (isLoading) return;
+  //   debugger;
+  //   if (data.length === 1 && approvedVendors?.length == 0) {
+  //     debugger;
+  //     logout();
+  //   }
+  // }, [data,isLoading]);
+  useEffect(() => {
     if (isLoading || isUserLoading) return;
     if (data.length >= 1 && user?.isApproved !== "1") {
       alert("Your account is not approved yet. Please contact support.");
       logout();
     }
-  }, [isLoading,isUserLoading]);
+  }, [isLoading, isUserLoading]);
   return (
     <Menu>
       <br />
@@ -74,38 +96,42 @@ export const MyMenu = () => {
       <hr />
       {hasVendors && data.length > 0 && (
         <>
-          <Menu.Item
-            sx={{ fontSize: "0.875rem" }}
-            to={"/vendor/"}
-            primaryText="Dashboard"
-            leftIcon={<FaChartArea size={22} />}
-          />
+          <CollapsibleMenu menuName="Dashboard 1">
+            <Menu.Item
+              sx={{ fontSize: "0.875rem" }}
+              to={"/vendor/"}
+              primaryText="Dashboard"
+              leftIcon={<FaChartArea size={22} />}
+            />
 
-          <Menu.Item
-            sx={{ fontSize: "0.875rem" }}
-            to="/vendor/package"
-            primaryText="Package"
-            leftIcon={<PiPackageBold size={22} />}
-          />
-          <Menu.Item
-            sx={{ fontSize: "0.875rem" }}
-            to="/vendor/booking"
-            primaryText="Booking"
-            leftIcon={<PiTicket size={22} />}
-          />
-
-          <Menu.Item
-            sx={{ fontSize: "0.875rem" }}
-            to={"/vendor/vendor/" + hasVendors}
-            primaryText="Edit Profile"
-            leftIcon={<PiUserCircleBold size={22} />}
-          />
-          <Menu.Item
-            sx={{ fontSize: "0.875rem" }}
-            to="/profile/forgot-password"
-            primaryText="Forget Password"
-            leftIcon={<PiLockKeyBold size={22} />}
-          />
+            <Menu.Item
+              sx={{ fontSize: "0.875rem" }}
+              to="/vendor/package"
+              primaryText="Package"
+              leftIcon={<PiPackageBold size={22} />}
+            />
+            <Menu.Item
+              sx={{ fontSize: "0.875rem" }}
+              to="/vendor/booking"
+              primaryText="Booking"
+              leftIcon={<PiTicket size={22} />}
+            />
+          </CollapsibleMenu>
+          <CollapsibleMenu menuName="Dashboard 2">
+            <Menu.Item
+              sx={{ fontSize: "0.875rem" }}
+              to={"/vendor/vendor/" + hasVendors}
+              primaryText="Edit Profile"
+              leftIcon={<PiUserCircleBold size={22} />}
+            />
+            <Menu.Item
+              sx={{ fontSize: "0.875rem" }}
+              to="/profile/forgot-password"
+              primaryText="Forget Password"
+              leftIcon={<PiLockKeyBold size={22} />}
+            />
+          </CollapsibleMenu>
+          <CollapsibleMenu menuName="Dashboard 3">
           <Menu.Item
             sx={{ fontSize: "0.875rem" }}
             to="/vendor/email"
@@ -118,6 +144,7 @@ export const MyMenu = () => {
             primaryText="Support Ticket"
             leftIcon={<FaBookMedical size={22} />}
           />
+          </CollapsibleMenu>
         </>
       )}
     </Menu>
